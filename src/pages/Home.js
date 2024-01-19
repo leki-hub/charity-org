4fe2c50c4a7bd81c1786ac299dd0a0ad102035e3
Leki <lekisintole82@gmail.com>
2024-01-19 11:12:38 +0300

    header section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,30 @@ const Home = () => {
          {/* -- Preloader -- */}
          <div className="preloader"></div>
                    {/* --Header top area--  */}
-                   <header className="main-header header-style-one">
+            <header className="main-header header-style-one">
+                   <section className="header-top">
+                <div className="container">
+                    <div className="header">
+                        <div className="header-top-left">
+                            <ul>
+                             <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                             <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                             <li><a href="#"><i className="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        </ul>
+                        </div>
+                        <div className="header-top-right">
+                            <ul>
+                                <li>
+                                    <a href="#"><span><i className="fa fa-envelope-o" aria-hidden="true"></i></span> kindness@yahoo.com</a></li>
+                                <li><p><span><i className="fa fa-phone" aria-hidden="true"></i></span>012.9355.121</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* --/header_top_area-- */}
 
-
-                    
-                   </header>
+        </header>
       </div>
            
   )
