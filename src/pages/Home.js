b1afb7f571f296bad01293e00fe187fc79843ff6
Leki <lekisintole82@gmail.com>
2024-01-24 21:40:51 +0300

    item 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -423,14 +423,16 @@ const Home = () => {
                                         {/* -- /progress-item -- */}
                                  <p>By supporting this initiative, you are supporting a generation.</p>
                                 <a href="#">Donate now</a>
-
-
-                          
                             </div>
+                        </div>
+                     </div>
+                     <div className="col-md-3 col-sm-6 col-xs-12">
+
+
+
                      </div>
 
-                 </div>
-
+                     
 
                </div>
 
