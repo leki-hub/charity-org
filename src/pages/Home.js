2fa0fe93c269ce6e4af240f43bf68d01e5be322d
Leki <lekisintole82@gmail.com>
2024-01-25 20:28:31 +0300

    item 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -548,7 +548,14 @@ const Home = () => {
                             </div>
                             {/* another single item */}
                             <div className="col-md-6 col-sm-6 col-xs-12">
-
+                            <div className="event-item">
+                                    <div className="img-holder">
+                                       <figure><a href="single-event.html"><img src="images/blog/6.jpg" alt="Images"/></a></figure>
+                                        <div className="text">
+                                            <h4><a href="single-event.html">October 5, 2017 <span>giving food</span></a></h4>
+                                        </div>
+                                    </div>
+                                </div>
 
 
 
