27f5d525917ce56046243b9fde1372c43edf08f4
Leki <lekisintole82@gmail.com>
2024-01-25 20:46:53 +0300

    navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -550,9 +550,9 @@ const Home = () => {
                             <div className="col-md-6 col-sm-6 col-xs-12">
                             <div className="event-item">
                                     <div className="img-holder">
-                                       <figure><a href="single-event.html"><img src="images/blog/6.jpg" alt="Images"/></a></figure>
+                                       <figure><Link to="/single-event"><img src="images/blog/6.jpg" alt="Images"/></Link></figure>
                                         <div className="text">
-                                            <h4><a href="single-event.html">October 5, 2017 <span>giving food</span></a></h4>
+                                            <h4><Link to="/single-event">October 5, 2017 <span>giving food</span></Link></h4>
                                         </div>
                                     </div>
                                 </div>
